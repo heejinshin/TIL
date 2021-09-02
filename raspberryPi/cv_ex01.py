f43add99ba0c426939b8b5ac8b15080486c447f4
heejinshin <86960303+heejinshin@users.noreply.github.com>
2021-09-03 00:34:36 +0900

    Add files via upload
    opencv, graphic

--- raspberryPi/cv_ex01.py
+++ raspberryPi/cv_ex01.py
@@ -1,12 +1,11 @@
+import numpy as np
 import cv2
 
-image_file = './data/lena.jpg'   #cmd기준 상대경로
-img = cv2.imread(image_file) # cv2.IMREAD_COLOR
-img2 = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
+#모두 0으로 되어 있는 빈 canvas(검정색)
+img = np.zeros((512, 512, 3), np.unit8) #튜플, 차원, 0으로 채우기, bgr(0, 0, 0)이면 검정색
+#검정색 캔버스를 하나 만든것, 첫번째 좌표 pt1, 두번째 pt2(우측하단좌표)
+cv2.line(img, (0, 0), (511, 511), (255, 0, 0), 5)
 
-
-print(img.shape, img2.shape)
-cv2.imshow('Lena color', img)
-cv2.imshow('Lena grayscale', img2)
-
-cv2.waitKey(1000) #ms
+cv2.imshow('image', img)
+cv2.waitKey(0)  #키 입력이 될때까지 무한 대기
+cv2.destroyAllWindows()
