e426f47ff3aac45480160dbd9ee44e0a500fcec5
heejinshin <86960303+heejinshin@users.noreply.github.com>
2021-09-03 00:49:29 +0900

    Add files via upload
    opencv circle, rectangle

--- raspberryPi/cv_ex02.py
+++ raspberryPi/cv_ex02.py
@@ -3,7 +3,8 @@ import cv2
 import numpy as np
 
 #white 배경 색상
-img = np.zeros(shape=(512, 512, 3), dtype=np.unit8)
+img = np.zeros(shape=(512, 512, 3), dtype=np.unit8) + 255  #white->255
+#unit: unsigned integer
 
 x1, x2 = 100, 400
 y1, y2 = 100, 400
@@ -12,5 +13,15 @@ pt1 = 120, 50
 pt2 = 300, 500
 cv2.line(img, pt1, pt2, (255, 0, 0), 2)
 
-imgRect = (x1, y1, x2-x1, y2-y1)
+imgRect = (x1, y1, x2-x1, y2-y1)  #width, height
 retval, rpt1, rpt2 = cv2.clipLine(imgRect, pt1, pt2)
+
+#교차점 원 그리기
+if retval: 
+    cv2.circle(img, rpt1, radius=5, color=(0, 255, 0), thickness=-1)
+    #두께가 음수인경우 원의 내부 채움
+    cv2.circle(img, rpt2, radius=5, color=(0, 255, 0), thickness=-1)
+
+cv2.imshow('img', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
