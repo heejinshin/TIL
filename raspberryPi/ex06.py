8da8995695bc3788e6b159d844d21e68bf2eb6d2
heejinshin <86960303+heejinshin@users.noreply.github.com>
2021-09-03 00:01:22 +0900

    Add files via upload
    비디오캡처기능

--- raspberryPi/ex06.py
+++ raspberryPi/ex06.py
@@ -1,7 +1,7 @@
 #버튼을 누르면 녹화 시작
 #버튼을 떼면 녹화 종료
 
-from gpiozero import Button
+from gpiozero import Button, MotionSensor
 from signal import pause
 import cv2
 from datetime import datetime
@@ -28,17 +28,20 @@ def stop_record():
         recorder.release()
         recorder = None
 
-button = Button(26)
+# button = Button(26)
+# button.when_pressed = start_record
+# button.when_released = stop_record
 
-button.when_pressed = start_record
-button.when_released = stop_record
+pir = MotionSensor(12)   #PIR 핵심기술 렌즈, 적외선 감지
+pir.when_motion = start_record
+pir.when_no_motion = stop_record
 
 while True:
     retval, frame = cap.read()   #프레임 캡처
     if not retval: break
 
-    if recording_status:   #if는 true일때 아래를 실행하니까 
-        recorder.write(frame)  #녹화중일 경우에만 frame 쓰기 처리 
+    if recording_status:   #녹화중인 경우 프레임 처리 
+        recorder.write(frame)  
 
     cv2.imshow('frame', frame)
 
