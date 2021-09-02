2159bb574a096563a71ea8c6230895a0d2c78712
heejinshin <86960303+heejinshin@users.noreply.github.com>
2021-09-03 00:42:39 +0900

    Add files via upload
    opencv method

---
+++ raspberryPi/cv_ex02.py
@@ -0,0 +1,16 @@
+#직선과 사각형의 교차점 리턴, clipLine()
+import cv2
+import numpy as np
+
+#white 배경 색상
+img = np.zeros(shape=(512, 512, 3), dtype=np.unit8)
+
+x1, x2 = 100, 400
+y1, y2 = 100, 400
+cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
+pt1 = 120, 50
+pt2 = 300, 500
+cv2.line(img, pt1, pt2, (255, 0, 0), 2)
+
+imgRect = (x1, y1, x2-x1, y2-y1)
+retval, rpt1, rpt2 = cv2.clipLine(imgRect, pt1, pt2)
