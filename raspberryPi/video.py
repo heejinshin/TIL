7a07f41692bb4abdc005e7529019ca53ffde198a
heejinshin <86960303+heejinshin@users.noreply.github.com>
2021-09-04 10:34:51 +0900

    Add files via upload

--- raspberryPi/video.py
+++ raspberryPi/video.py
@@ -19,3 +19,36 @@ class Video:
             return image #이미지 리턴
         else:
             raise StopIteration  #예외처리, for문 종료
+
+    def __enter__(self): #context manager
+        return self
+    
+    def __exit__(self, type, value, trace_back): #self외 는 안씀
+        if self.cap and self.cap.inOpened(): #장치가 있고, 장치가 열려있으면
+            # print('video release-----') #확인용 print
+            self.cap.release() #release
+        cv2.destroyAllWindows()
+        
+
+
+#static메서드_인스턴스로 접근하지 않고 클래스로 접근하겠다는 뜻, Video.show()
+    @staticmethod  #self쓰지x
+    def show(image, exit_char=ord('q')): #ord()-> 문자의 아스키코드리턴
+        cv2.imshow('frame', image)
+        if cv2.waitKey(1) & 0xFF == exit_char: #종료하는 문자인가
+            return False #종료선언
+        return True #계속 진행
+    
+    @staticmethod
+    def to_jpg(frame, quality=80):
+        encode_param=[int(cv2.IMWRITE_JPEG_QUALITY), quality]
+        is_seccess, jpg = cv2.imencode(".jpg", frame, encode_param)
+        return jpg
+
+if __name__ == '__main__':
+    with Video(device=2) as video: #__enter__()호출
+        for image in video:
+            if not Video.show(image, 27): break
+            #if not Video.show(image): break #q로 종료
+
+    #with 블럭 벗어날 때 __exit__()호출
